990bb4e35545657a45e76b564713f428cdc3af81
codeknight05 <khandelwalyash.ajay2022@vitstudent.ac.in>
2024-11-17 17:27:05 +0530

    changed routers

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,19 +1,23 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/codeknight05/GO-senior-care/pkg/controllers"
+	"database/sql"
 
+	"github.com/codeknight05/GO-senior-care/pkg/config"
+	"github.com/codeknight05/GO-senior-care/pkg/controllers"
+	"github.com/gofiber/fiber/v2"
 )
+var Db *sql.DB
 
 func Setup(app *fiber.App) {
 
-	app.GET("/health/medications", controllers.ge)
-	app.POST("/health/medications",)
-	app.GET("/caregiver/status", )
-	app.POST("/caregiver/status", )
-	app.GET("/admin/users", )
-	app.POST("/admin/users", )
-	app.POST("/emergency/alerts", )
+	Db=config.GetDB()
+	app.Get("/health/medications", controllers.GetMedications(Db))
+	app.Post("/health/medications",controllers.AddMedication(Db))
+	app.Get("/caregiver/status", controllers.GetCaregiverStatus(Db))
+	app.Post("/caregiver/status", controllers.UpdateCaregiverStatus(Db))
+	app.Get("/admin/users", controllers.GetUsers(Db))
+	app.Post("/admin/users", controllers.AddUser(Db))
+	app.Post("/emergency/alerts", controllers.SendEmergencyAlert())
 }
 
